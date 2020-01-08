2898ac0e1f1ef0bdf3cf9d28fee5dfe965efb22e
Saravanan Gnanaguru <saravanan.79@wipro.com>
2020-01-08 15:51:05 +0530

    repo code added

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+package 'Install Apache' do
+  case node['platform']
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
